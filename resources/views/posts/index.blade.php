3ef3b9e604de220c96781fff1831e2f408692d53
Fua-0624 <fua.sugimura.kochi@gmail.com>
2024-03-31 15:27:47 +0900

    詳細画面の追加

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!--Fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <div class="posts">
+            @foreach($posts as $post)
+                <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                <p class="body">{{ $post->body }}</p>
+            @endforeach
+        </div>
+        <div class="paginate">
+            {{ $posts->links() }}
+        </div>
+    </body>
+</html>
